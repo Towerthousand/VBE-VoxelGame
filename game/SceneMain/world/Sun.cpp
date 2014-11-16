06e01b97b4d04d18179f7e743f3c736dda38a77d
Towerthousand <dani.torramilans@gmail.com>
2014-11-16 19:39:42 +0100

    Working again, but with old AABB. Cleaned up and will fix it sometime

--- game/SceneMain/world/Sun.cpp
+++ game/SceneMain/world/Sun.cpp
@@ -5,21 +5,22 @@
 
 Sun::Sun() : angle(45.0f) {
 	setName("sun");
+	minZ = std::vector<float>(NUM_SUN_CASCADES, 0.0f);
 	maxZ = std::vector<float>(NUM_SUN_CASCADES, 0.0f);
 	float zFar = std::min(WORLDSIZE*CHUNKSIZE, 256);
-	float numParts = pow(2, NUM_SUN_CASCADES) - 1;
+	float numParts = (1 << (NUM_SUN_CASCADES)) - 1;
 	float lastMax = 0.0f;
-	globalCam = new Camera();
-	globalCam->projection = glm::ortho(-10,10,-10,10,-100,100);
-	globalCam->addTo(this);
 	for(int i = 0; i < NUM_SUN_CASCADES; ++i) {
 		cameras[i] = new Camera();
 		cameras[i]->projection = glm::ortho(-10,10,-10,10,-100,100);
 		cameras[i]->addTo(this);
 		minZ[i] = lastMax;
-		maxZ[i] = zFar*(pow(2,i)/numParts);
+		maxZ[i] = zFar*((1 << i)/numParts);
 		lastMax = maxZ[i];
 	}
+	cameras[NUM_SUN_CASCADES] = new Camera();
+	cameras[NUM_SUN_CASCADES]->projection = glm::ortho(-10,10,-10,10,-100,100);
+	cameras[NUM_SUN_CASCADES]->addTo(this);
 	minZ[0] = -10000;
 	maxZ[NUM_SUN_CASCADES-1] = zFar;
 	VP = std::vector<mat4f>(NUM_SUN_CASCADES, mat4f(1.0f));
@@ -35,19 +36,32 @@ void Sun::update(float deltaTime) {
 }
 
 void Sun::updateCameras() {
-	AABB globalAABB = AABB();
-	for(int i = 0; i < NUM_SUN_CASCADES; ++i) {
-		numOccluders[i] = 0;
-		calculateAABB(i);
-		updateSingleCam(cameras[i], aabbs[i]);
-		VP[i] = cameras[i]->projection*cameras[i]->getView();
-		if(numOccluders[i] != 0) globalAABB.extend(aabbs[i]);
+	recalculateAABBs();
+	for(unsigned int i = 0; i < NUM_SUN_CASCADES+1; ++i) {
+		cameras[i]->pos = aabbs[i].getCenter();
+		cameras[i]->lookInDir(getDirection());
+		min[i] = vec3f(std::numeric_limits<float>::max());
+		max[i] = vec3f(std::numeric_limits<float>::lowest());
+		extend({i}, aabbs[i]);
 	}
-	updateSingleCam(globalCam, globalAABB);
+	for(int i = 0; i < NUM_SUN_CASCADES+1; ++i) {
+		cameras[i]->pos -= cameras[i]->getForward()*(glm::abs(min[i].z));
+		max[i].z += glm::abs(min[i].z);
+		cameras[i]->projection = glm::ortho(min[i].x, max[i].x, min[i].y, max[i].y, min[i].z, max[i].z);
+		cameras[i]->recalculateFrustum();
+	}
+	for(int i = 0; i < NUM_SUN_CASCADES; ++i) {
+		VP[i] = cameras[i]->projection*cameras[i]->getView();
+	}
 }
 
-void Sun::calculateAABB(unsigned int camID) {
-	aabbs[camID] = AABB();
+void Sun::recalculateCameras() {
+	for(int i = 0; i < NUM_SUN_CASCADES+1; ++i) {
+		cameras[i]->pos = aabbs[i].getCenter();
+		cameras[i]->lookInDir(getDirection());
+		min[i] = vec3f(std::numeric_limits<float>::max());
+		max[i] = vec3f(std::numeric_limits<float>::lowest());
+	}
 	Camera* pCam = (Camera*)getGame()->getObjectByName("playerCam");
 	World* w = (World*)getGame()->getObjectByName("world");
 	for(int x = 0; x < WORLDSIZE; ++x)
@@ -57,20 +71,51 @@ void Sun::calculateAABB(unsigned int camID) {
 			for(unsigned int y = 0; y < col->getChunkCount(); ++y) {
 				Chunk* actual = col->getChunkCC(y);
 				if(actual != nullptr && actual->wasDrawedByPlayer()) {
-					float dist = glm::length(vec3f(pCam->getView() * vec4f(actual->getWorldSpaceBoundingBox().getCenter(), 1.0f)));
-					if(dist < maxZ[camID]+CHUNKSIZE*1.73 && dist > minZ[camID]-CHUNKSIZE*1.73) {
-						aabbs[camID].extend(actual->getWorldSpaceBoundingBox());
-						numOccluders[camID]++;
+					std::vector<unsigned int> indexes;
+					for(int i = 0; i < NUM_SUN_CASCADES; ++i) {
+						float dist = glm::abs(vec3f(pCam->getView() * vec4f(actual->getWorldSpaceBoundingBox().getCenter(), 1.0f)).z);
+						if(dist < maxZ[i]+CHUNKSIZE*1.73 && dist > minZ[i]-CHUNKSIZE*1.73)
+							indexes.push_back(i);
+					}
+					if(!indexes.empty()) {
+						indexes.push_back(NUM_SUN_CASCADES);
+						extend(indexes, actual->getWorldSpaceBoundingBox());
 					}
 				}
 			}
 		}
 }
-void Sun::updateSingleCam(Camera* cam, const AABB& occludedBox) {
-	cam->pos = occludedBox.getCenter();
-	cam->lookInDir(getDirection());
 
-	vec3f min(std::numeric_limits<float>::max()), max(std::numeric_limits<float>::min());
+void Sun::recalculateAABBs() {
+	for(int i = 0; i < NUM_SUN_CASCADES+1; ++i) aabbs[i] = AABB();
+	Camera* pCam = (Camera*)getGame()->getObjectByName("playerCam");
+	World* w = (World*)getGame()->getObjectByName("world");
+	for(int x = 0; x < WORLDSIZE; ++x)
+		for(int z = 0; z < WORLDSIZE; ++z) {
+			Column* col = w->columns[x][z];
+			if(col == nullptr) continue;
+			for(unsigned int y = 0; y < col->getChunkCount(); ++y) {
+				Chunk* actual = col->getChunkCC(y);
+				if(actual != nullptr && actual->wasDrawedByPlayer()) {
+					bool include = false;
+					for(int i = 0; i < NUM_SUN_CASCADES; ++i) {
+						float dist = glm::abs(vec3f(pCam->getView() * vec4f(actual->getWorldSpaceBoundingBox().getCenter(), 1.0f)).z);
+						if(dist < maxZ[i]+CHUNKSIZE*1.73 && dist > minZ[i]-CHUNKSIZE*1.73) {
+							aabbs[i].extend(actual->getWorldSpaceBoundingBox());
+							numOccluders[i]++;
+							include = true;
+						}
+					}
+					if(include) {
+						aabbs[NUM_SUN_CASCADES].extend(actual->getWorldSpaceBoundingBox());
+						numOccluders[NUM_SUN_CASCADES]++;
+					}
+				}
+			}
+		}
+}
+
+void Sun::extend(std::vector<unsigned int> index, const AABB& occludedBox) {
 	vec3f projections[8];
 	projections[0] = occludedBox.getMin();
 	projections[1] = occludedBox.getMin() + occludedBox.getDimensions()*vec3f(0, 0, 1);
@@ -81,17 +126,15 @@ void Sun::updateSingleCam(Camera* cam, const AABB& occludedBox) {
 	projections[6] = occludedBox.getMin() + occludedBox.getDimensions()*vec3f(1, 1, 0);
 	projections[7] = occludedBox.getMin() + occludedBox.getDimensions();
 	for(int i = 0; i < 8; ++i) {
-		vec3f& p = projections[i];
-		p = vec3f(cam->getView()*vec4f(p, 1.0f)); //project this corner onto the
-		max.x = std::max(max.x, p.x);
-		min.x = std::min(min.x, p.x);
-		max.y = std::max(max.y, p.y);
-		min.y = std::min(min.y, p.y);
-		max.z = std::max(max.z, p.z);
-		min.z = std::min(min.z, p.z);
+		for(unsigned int j : index) {
+			vec3f p = projections[i];
+			p = vec3f(cameras[j]->getView()*vec4f(p, 1.0f)); //project this corner onto the
+			max[j].x = std::max(max[j].x, p.x);
+			min[j].x = std::min(min[j].x, p.x);
+			max[j].y = std::max(max[j].y, p.y);
+			min[j].y = std::min(min[j].y, p.y);
+			max[j].z = std::max(max[j].z, p.z);
+			min[j].z = std::min(min[j].z, p.z);
+		}
 	}
-	cam->pos -= cam->getForward()*(glm::abs(min.z));
-	max.z += glm::abs(min.z);
-	cam->projection = glm::ortho(min.x, max.x, min.y, max.y, min.z, max.z);
-	cam->recalculateFrustum();
 }
--- game/SceneMain/world/Sun.hpp
+++ game/SceneMain/world/Sun.hpp
@@ -16,21 +16,23 @@ class Sun : public GameObject {
 		float getAngle() const {return angle;}
 
 		const Camera* getCam(unsigned int i) const {return cameras[i];}
-		const Camera* getGlobalCam() const {return globalCam;}
+		const Camera* getGlobalCam() const {return cameras[NUM_SUN_CASCADES];}
 		const std::vector<float>& getDepthPlanes() const {return maxZ;}
 		const std::vector<mat4f>& getVPMatrices() const {return VP;}
 
 	private:
-		void calculateAABB(unsigned int camID);
-		void updateSingleCam(Camera* cam, const AABB& occludedBox);
+		void recalculateCameras();
+		void recalculateAABBs();
+		void extend(std::vector<unsigned int> index, const AABB& occludedBox);
 
 		float angle; //sun always moves on the x-y plane (z never changes)
-		Camera* cameras[NUM_SUN_CASCADES];
-		Camera* globalCam;
-		float minZ[NUM_SUN_CASCADES];
+		vec3f min[NUM_SUN_CASCADES+1];
+		vec3f max[NUM_SUN_CASCADES+1];
+		Camera* cameras[NUM_SUN_CASCADES+1];
+		AABB aabbs[NUM_SUN_CASCADES+1];
+		int numOccluders[NUM_SUN_CASCADES+1];
+		std::vector<float> minZ;
 		std::vector<float> maxZ;
-		AABB aabbs[NUM_SUN_CASCADES];
-		int numOccluders[NUM_SUN_CASCADES];
 		std::vector<mat4f> VP;
 };
 
