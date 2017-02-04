90068a09287fecbfe3392ea1211c1809e1d590d6
Towerthousand <dani.torramilans@gmail.com>
2017-02-04 20:08:12 +0100

    Added biome enum + GenParams operators

--- game/SceneMain/world/generator/GenParams.hpp
+++ game/SceneMain/world/generator/GenParams.hpp
@@ -1,9 +1,80 @@
 #ifndef GENPARAMS3D_HPP
 #define GENPARAMS3D_HPP
 
-struct GenParams {
-    float hi = 0;
-    float lo = 0;
+enum Biome {
+    OCEAN = 0,
+    PLAINS,
+    NUM_BIOMES
 };
+
+struct GenParams {
+    float lo;
+    float hi;
+    float scale;
+
+    GenParams& operator+=(const GenParams& p) {
+        lo += p.lo;
+        hi += p.hi;
+        scale += p.scale;
+
+        return *this;
+    };
+
+    GenParams& operator-=(const GenParams& p) {
+        lo -= p.lo;
+        hi -= p.hi;
+        scale -= p.scale;
+
+        return *this;
+    };
+
+    GenParams& operator*=(const float& f) {
+        lo *= f;
+        hi *= f;
+        scale *= f;
+
+        return *this;
+    };
+
+    GenParams& operator/=(const float& f) {
+        lo /= f;
+        hi /= f;
+        scale /= f;
+
+        return *this;
+    };
+};
+
+GenParams operator+(const GenParams& a, const GenParams& b) {
+    return {
+        a.lo+b.lo,
+        a.hi+b.hi,
+        a.scale+b.scale
+    };
+}
+
+GenParams operator-(const GenParams& a, const GenParams& b) {
+    return {
+        a.lo-b.lo,
+        a.hi-b.hi,
+        a.scale-b.scale
+    };
+}
+
+GenParams operator*(const GenParams& a, const float& b) {
+    return {
+        a.lo*b,
+        a.hi*b,
+        a.scale*b
+    };
+}
+
+GenParams operator/(const GenParams& a, const float& b) {
+    return {
+        a.lo/b,
+        a.hi/b,
+        a.scale/b
+    };
+}
 
 #endif // GENPARAMS3D_HPP
