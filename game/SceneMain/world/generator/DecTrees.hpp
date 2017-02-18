520b663323f05cbb9bd025dff8389652632d3754
Towerthousand <dani.torramilans@gmail.com>
2017-02-18 21:04:20 +0100

    Make DecTrees take into account current biome

--- game/SceneMain/world/generator/DecTrees.hpp
+++ game/SceneMain/world/generator/DecTrees.hpp
@@ -3,21 +3,20 @@
 #include "Dec.hpp"
 #include "Noise2D.hpp"
 
-#define DECTREES_MIN_GRID_SIZE 2
 #define DECTREES_MAX_GRID_SIZE 4
-#define DECTREES_GRID_SIZE_CUTOFF 5
-#define DECTREES_DROP_CHANCE 0.5f
-#define DECTREES_MIN_POWER -0.5f
-#define DECTREES_MAX_POWER 0.5f
 
 class DecTrees final : public Dec {
     public:
-        DecTrees(std::mt19937* rng) :
-            gridNoise(rng, 0.0f, 1.0f, 60.0f),
-            dispNoiseX(rng, DECTREES_MIN_POWER, DECTREES_MAX_POWER, 0.1f),
-            dispNoiseY(rng, DECTREES_MIN_POWER, DECTREES_MAX_POWER, 0.1f),
-            dropNoise(rng, 0.0f, 1.0f, 100.0f),
-            genNoise(rng, 0.0f, 1.0f, 1.0f) {}
+        DecTrees(std::mt19937* rng, BiomeParam minGrid, BiomeParam maxGrid, BiomeParam gridCutoff, BiomeParam dropChance) :
+            minGrid(minGrid),
+            maxGrid(maxGrid),
+            gridCutoff(gridCutoff),
+            dropChance(dropChance),
+            gridNoise(rng),
+            dispNoiseX(rng),
+            dispNoiseY(rng),
+            dropNoise(rng),
+            genNoise(rng) {}
         virtual ~DecTrees() {}
 
         void decorate(ColumnGenerator::ColumnData* col) {
@@ -29,10 +28,10 @@ class DecTrees final : public Dec {
         void genTree(ColumnGenerator::ColumnData* col, const vec2i& pos) {
             // Create a generator for this specific world pos
             vec2i wPos = col->pos*CHUNKSIZE + pos;
-            unsigned int seed = std::numeric_limits<unsigned int>::max()*genNoise.get(wPos.x, wPos.y);
+            unsigned int seed = std::numeric_limits<unsigned int>::max()*getNoise(&genNoise, wPos.x, wPos.y, 0.0f, 1.0f, 1.0f);
             std::mt19937 gen(seed);
 
-            int top = (*col->raw)[0].size();
+            int top = GENERATIONHEIGHT*CHUNKSIZE;
 
             // Tree height
             int tHeight = 8+gen()%8;
@@ -40,7 +39,7 @@ class DecTrees final : public Dec {
             // Find Base
             unsigned int base = 0;
             for(int i = top-1; i >= 0; --i)
-                if((*col->raw)[pos.x][i][pos.y] != 0) {
+                if(col->raw[pos.x*CHUNKSIZE*CHUNKSIZE*GENERATIONHEIGHT+pos.y*CHUNKSIZE*GENERATIONHEIGHT+i] != 0) {
                     base = i;
                     break;
                 }
@@ -124,29 +123,32 @@ class DecTrees final : public Dec {
             }
         }
 
-        std::vector<vec2i> getTreePositions(ColumnGenerator::ColumnData* col) {
+        std::vector<vec2i> getTreePositions(const ColumnGenerator::ColumnData* col) {
             std::vector<vec2i> trees;
             vec2i offset = col->pos*CHUNKSIZE;
             int margin = (1 << DECTREES_MAX_GRID_SIZE);
             for(int i = -margin; i < CHUNKSIZE+margin; ++i)
                 for(int j = -margin; j < CHUNKSIZE+margin; ++j) {
+                    // Get the generation params for this spot's biome
+                    Biome b = col->biomes[(i+BIOME_MATRIX_MARGIN)*BIOME_MATRIX_SIZE+(j+BIOME_MATRIX_MARGIN)];
+                    const std::valarray<float>* params = &ColumnGenerator::genParams[b];
                     vec2i c = vec2i(i,j) + offset;
                     // DECTREES_MIN_GRID_SIZE <= gridsize <= DECTREES_MAX_GRID_SIZE
-                    int gridsize = 1 << int(floor(DECTREES_MIN_GRID_SIZE+gridNoise.octavedGet(c.x, c.y, 4)*((DECTREES_MAX_GRID_SIZE+1)-DECTREES_MIN_GRID_SIZE)));
+                    int gridsize = 1 << int(floor((*params)[minGrid]+getNoise(&gridNoise, c.x, c.y, 0.0f, 1.0f, 60.0f, 4)*(((*params)[maxGrid]+1)-(*params)[minGrid])));
 
                     // Gridsize too big
-                    if(gridsize > (1 << DECTREES_GRID_SIZE_CUTOFF))
+                    if(gridsize > (1 << int(glm::floor((*params)[gridCutoff]))))
                         continue;
                     // For this gridsize, this is not the adequate
                     // position to displace
                     if(c%gridsize != vec2i(0))
                         continue;
                     // Random drop to make it look more patched
-                    if(dropNoise.octavedGet(c.x, c.y, 4) < DECTREES_DROP_CHANCE)
+                    if(getNoise(&dropNoise, c.x, c.y, 0.0f, 1.0f, 100.0f, 4) < (*params)[dropChance])
                         continue;
 
                     // Random displacement
-                    vec2f disp = vec2f(dispNoiseX.get(c.x,c.y)*gridsize-1, dispNoiseY.get(c.x,c.y)*gridsize-1);
+                    vec2f disp = vec2f(getNoise(&dispNoiseX, c.x, c.y, -0.5f, 0.5f, 0.1f)*gridsize-1, getNoise(&dispNoiseY, c.x, c.y, -0.5f, 0.5f, 0.1f)*gridsize-1);
 
                     // The final position of the tree
                     vec2i p = c - offset + vec2i(glm::round(disp));
@@ -157,7 +159,16 @@ class DecTrees final : public Dec {
                 }
             return trees;
         }
+
     private:
+        float getNoise(Noise2D* noise, float x, float y, float min, float max, float scale, int octaves=1) {
+            return min+(max-min)*noise->octavedGet(x/scale, y/scale, octaves);
+        }
+
+        BiomeParam minGrid;
+        BiomeParam maxGrid;
+        BiomeParam gridCutoff;
+        BiomeParam dropChance;
         Noise2D gridNoise;
         Noise2D dispNoiseX;
         Noise2D dispNoiseY;
